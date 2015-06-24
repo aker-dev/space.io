42d906e278618fc666739b0c0af4dd89b55429af
Adrien Revel <adrien@abrege.net>
2015-06-24 11:22:41 +0200

    Refactor JS

--- public/scripts.js
+++ public/scripts.js
@@ -1,10 +1,7 @@
-	// init Isotope
-	var $grid = $('.grid').isotope({
-	});
-
+// init Isotope
+var $grid = $('.grid').isotope();
 
 $(function() {
-
 
 	$grid.isotope({
 		itemSelector: '.card',
@@ -13,9 +10,9 @@ $(function() {
 			// use outer width of grid-sizer for columnWidth
 			columnWidth: '.col-md-3'
 		  }
-	})
+	});
 
-			// filtres isotopes sur les boutons
+	// filtres isotopes sur les boutons
 	$('.btn-tag').click(function(event) {
 		if ($(this).hasClass( "tag-id-all" )) {
 			$grid.isotope({ filter: '*' });
@@ -25,46 +22,21 @@ $(function() {
 		}
 	});
 
-			// ouvre page dans le modal
-		$('.btn-page').click(function(event) {
-			if ($(this).attr( "data-dest-url")) {
-				var destUrl = $(this).attr( "data-dest-url");
-				$('.card').fadeOut( function(){
-
-					$('.modal-body').load(destUrl, function(){
-						
-					});
-					
-					$('.modal-page').modal('show');	
-				});
-
-				
+	// ouvre page dans le modal
+	$('.btn-page').click(function(event) {
+		if ($(this).attr( "data-dest-url")) {
+			var destUrl = $(this).attr( "data-dest-url");
+			$('.modal-body').load(destUrl);
+			$('.modal-page').modal('show');
 		}
-
 	});
 
-			// décharge page dans le modal
-			$('.modal-page').on('hidden.bs.modal', function (e) {
-			  // do something...
-
-
-		
-			$('.card').fadeIn( function(){
-					$('.modal-body').html('');
-					$grid.isotope({ filter: '*' });
-			});
-
-
-	
-			})
-
-			/* ferme modal
-		$('.btn-home').click(function(event) {
-				$('#page').modal('hide');
-
+	// décharge page dans le modal
+	$('.modal-page').on('hidden.bs.modal', function (e) {
+		$('.modal-body').html('');
+		$grid.isotope({ filter: '*' });
 	});
 
-*/
 });
             
 
