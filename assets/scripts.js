5939575a2f7c82990a0a8e182d3a61f33d62a316
Adrien Revel <adrien@abrege.net>
2015-06-25 13:33:35 +0200

    Published files v 0.2

---
+++ assets/scripts.js
@@ -0,0 +1,46 @@
+// init Isotope
+var $grid = $('.grid').isotope();
+
+$(function() {
+
+	$grid.isotope({
+		itemSelector: '.card',
+			percentPosition: true,
+		 	masonry: {
+			// use outer width of grid-sizer for columnWidth
+			columnWidth: '.col-md-3'
+		  }
+	});
+	$grid.isotope('shuffle');
+
+	// filtres isotopes sur les boutons
+	$('.btn-tag').click(function(event) {
+		if ($(this).hasClass( "tag-id-all" )) {
+			$grid.isotope({ filter: '*' });
+			$grid.isotope('shuffle');
+		}
+		if ($(this).attr( "data-tag-id")) {
+			$grid.isotope({ filter: '.tag-id-' + $(this).attr( "data-tag-id") });
+		}
+	});
+
+	// ouvre page dans le modal
+	$('.btn-page').click(function(event) {
+		if ($(this).attr( "data-dest-url")) {
+			var destUrl = $(this).attr( "data-dest-url");
+			$('.modal-body').load(destUrl);
+			$('.modal-page').modal('show');
+		}
+	});
+
+	// décharge page dans le modal
+	$('.modal-page').on('hidden.bs.modal', function (e) {
+		$('.modal-body').html('');
+	//	$grid.isotope({ filter: '*' });
+	});
+
+});
+            
+
+
+
